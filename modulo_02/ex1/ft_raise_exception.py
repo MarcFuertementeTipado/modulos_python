55c7258f38b59df0d429839546f4d98a58a80593
Marc Bosch <mbosch@car10s3.42barcelona.com>
2026-07-02 17:00:41 +0200

    ex1 hecho y ex0 corregido

--- modulo_02/ex1/ft_raise_exception.py
+++ modulo_02/ex1/ft_raise_exception.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 def input_temperature(temp_str: str) -> int:
-    return int(temp_str)
+    temp: int = int(temp_str)
+    if temp > 40:
+        raise ValueError(f'Caught input_temperature error: '
+                         f'{temp}°C is too hot for plants (max 40°C)')
+    if temp < 0:
+        raise ValueError(f'Caught input_temperature error: '
+                         f'{temp}°C is too cold for plants (min 0°C)')
+
+    else:
+        return temp
+
 
 def test_temperature() -> None:
-    test_cases = ["25", "abc"]
+    test_cases = ["25", "abc", "100", "-50"]
     print('=== Garden Temperature ===')
     for case in test_cases:
         try:
@@ -11,11 +21,10 @@ def test_temperature() -> None:
             num: int = input_temperature(case)
             print(f'Temperature is now {num}°C')
         except ValueError as er:
-            print(f'{er}')
+            print(f"{er}")
         except Exception:
             print('Error!')
     print('\nAll test completed - program didn\'t crash!')
 
 if __name__ == "__main__":
     test_temperature()
-
