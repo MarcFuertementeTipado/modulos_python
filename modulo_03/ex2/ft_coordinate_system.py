482e0d6021d5406282534bddee3ee2b26e5873de
Marc Bosch <mbosch@cbr10s2.42barcelona.com>
2026-07-13 13:28:47 +0200

    avanzando ex2

---
+++ modulo_03/ex2/ft_coordinate_system.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import math
+
+
+def get_player_pos() -> None:
+    temp: float = []
+    for i in range(1, 4):
+        while True:
+            try:
+                temp.append(round(float(input(f'introduce {i}: ')), 2))
+                break
+            except ValueError:
+                print('Invalid syntax')
+    coord = (temp[0], temp[1], temp[2])
+    return (coord)
+
+
+def calculate_dist(c1, c2=(0, 0, 0)) -> float:
+    distance = math.sqrt((c1[0] - c2[0])**2 + (c1[1] - c2[1])**2
+                         + (c1[2] - c2[2])**2)
+    return (distance)
+
+
+if __name__ == "__main__":
+    print('=== Game Coordinate System ===\n')
+    c1 = get_player_pos()
+    print("\nEnter new coordinates as floats informat 'x,y,z': "
+          f"{c1[0]} , {c1[1]} , {c1[2]}")
+    print(f'Got a first tuple: {c1}')
+    print(f'It includes: X={c1[0]}, Y={c1[1]}, Z={c1[2]}')
+    distance = calculate_dist(c1)
+    print(f'Distance to center: {round(distance, 4)}')
