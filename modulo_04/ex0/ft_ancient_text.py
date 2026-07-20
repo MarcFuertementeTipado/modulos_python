eb73a06d3ff7fa88c337b7eca1d842cbd9cdea94
Marc Bosch <mbosch@car11s2.42barcelona.com>
2026-07-20 16:01:45 +0200

    mejorado ex0

--- modulo_04/ex0/ft_ancient_text.py
+++ modulo_04/ex0/ft_ancient_text.py
@@ -3,13 +3,6 @@ import sys
 import typing
 
 
-def leer_archivo(archivo: typing.IO) -> None:
-    contenido = archivo.read()
-    print('---\n')
-    print(contenido)
-    print('---\n')
-
-
 def main():
     if len(sys.argv) < 2:
         print('Usage: ./ft_ancient_text.py <file>')
@@ -17,9 +10,11 @@ def main():
     try:
         print('=== Cyber Archives Recovery ===')
         print(f"Accessing file {sys.argv[1]}")
-        archivo_abierto = open(sys.argv[1], 'r')
-        leer_archivo(archivo_abierto)
-        archivo_abierto.close()
+        with open(sys.argv[1], 'r') as archivo:
+            contenido = archivo.read()
+            print('---\n')
+            print(contenido)
+            print('---\n')
         print(f"File '{sys.argv[1]}' closed.")
     except FileNotFoundError as ex:
         print(f"Error opening file '{sys.argv[1]}': ", ex)
