ad778b0c8dd19493bd1b21c243c4402702b505d3
marc <bose.marc13@proton.me>
2026-07-22 17:35:15 +0200

    solo falta el main

--- modulo_05/ex0/data_processor.py
+++ modulo_05/ex0/data_processor.py
@@ -3,28 +3,42 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 class DataProcessor(ABC):
+    def __init__(self):
+        self._items: list[str] = []
+        self.__rank_counter = 0
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
         """Check wether the input 
         data are appropriate for the current 
         data processor"""
         pass
-    # @abstractmethod
-    # def ingest(self, data: Any) -> None:
-    #     """process the input data"""
-    #     pass
-    # def output(self) -> tuple[int, str]:
-    #     pass
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        """process the input data"""
+        if not self.validate(data):
+            raise ValueError("Improper data")
+        
+    def output(self) -> tuple[int, str]:
+        if not self._items:
+            return None
+        self.__rank_counter += 1
+        return (self._items.pop(0), self.__rank_counter)
 
 
 class NumericProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         if isinstance(data, (int, float)):
             return True
-        if isinstance(data, tuple):
+        if isinstance(data, list):
             return all(isinstance(x, (int, float))for x in data)
         else:
             return False
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        super().ingest(data)
+        elementos = data if isinstance(data, list) else [data]
+        for elem in elementos:
+            self._items.append(str(elem))
 
 
 class TextProcessor(DataProcessor):
@@ -35,6 +49,11 @@ class TextProcessor(DataProcessor):
             return all(isinstance(x, str) for x in data)
         else:
             return False
+    def ingest(self, data: str | list[str]) -> None:
+        super().ingest(data)
+        elemento = data if isinstance(data, list) else [data]
+        for elem in elemento:
+            self._items.append(elem)
 
 
 class LogProcessor(DataProcessor):
@@ -43,15 +62,28 @@ class LogProcessor(DataProcessor):
             return all(isinstance(k, str) and isinstance(v, str)  for k, v in data.items())
         if isinstance(data, list):
             return all(isinstance(x, str) for x in data)
+        else:
+            return False
+
+
+    def ingest(self, data: dict[str,str] | list[dict[str, str]]) -> None:
+        super().ingest(data)
+        elementos = [data] if isinstance(data, dict) else data
+        for elem in elementos:
+            text = ": ".join(elem.values())
+            self._items.append(text)
+        pass
+
+
+
+def main():
+    
+    try:
+        numerico = NumericProcessor()
+        numerico.ingest("aba")
+    except ValueError as ex:
+        print(ex)
 
 
 if __name__ == "__main__":
-    numerico = NumericProcessor()
-    print(numerico.validate((4, 5.5)))
-
-    letras = TextProcessor()
-    print(letras.validate(("hola", "adios")))
-
-    diccionario = LogProcessor()
-    print(diccionario.validate(["hola", "adios"]))
-    
+    main()
