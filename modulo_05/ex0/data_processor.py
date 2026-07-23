19045b4c5344925c06dd106828817b9a2a86ba2b
Marc Bosch <mbosch@car5s6.42barcelona.com>
2026-07-23 15:08:01 +0200

    ex0 completado con flake8 y mypy

--- modulo_05/ex0/data_processor.py
+++ modulo_05/ex0/data_processor.py
@@ -2,6 +2,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class DataProcessor(ABC):
     def __init__(self):
         self._items: list[str] = []
@@ -9,17 +10,17 @@ class DataProcessor(ABC):
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
-        """Check wether the input 
-        data are appropriate for the current 
-        data processor"""
+        """Check whether the input data are appropriate for the current
+        data processor."""
         pass
+
     @abstractmethod
     def ingest(self, data: Any) -> None:
-        """process the input data"""
+        """Process the input data."""
         if not self.validate(data):
             raise ValueError("Improper data")
-        
-    def output(self) -> tuple[int, str]:
+
+    def output(self) -> tuple[str, int] | None:
         if not self._items:
             return None
         self.__rank_counter += 1
@@ -31,10 +32,11 @@ class NumericProcessor(DataProcessor):
         if isinstance(data, (int, float)):
             return True
         if isinstance(data, list):
-            return all(isinstance(x, (int, float))for x in data)
-        else:
-            return False
-    def ingest(self, data: int | float | list[int | float]) -> None:
+            return all(isinstance(x, (int, float)) for x in data)
+        return False
+
+    def ingest(self, data: int | float | list[int]
+               | list[float] | list[int | float]) -> None:
         super().ingest(data)
         elementos = data if isinstance(data, list) else [data]
         for elem in elementos:
@@ -47,8 +49,8 @@ class TextProcessor(DataProcessor):
             return True
         if isinstance(data, list):
             return all(isinstance(x, str) for x in data)
-        else:
-            return False
+        return False
+
     def ingest(self, data: str | list[str]) -> None:
         super().ingest(data)
         elemento = data if isinstance(data, list) else [data]
@@ -59,27 +61,26 @@ class TextProcessor(DataProcessor):
 class LogProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         if isinstance(data, dict):
-            return all(isinstance(k, str) and isinstance(v, str)  for k, v in data.items())
+            return all(
+                isinstance(k, str) and isinstance(v, str)
+                for k, v in data.items()
+            )
         if isinstance(data, list):
             return all(isinstance(x, dict) for x in data)
-        else:
-            return False
+        return False
 
-
-    def ingest(self, data: dict[str,str] | list[dict[str, str]]) -> None:
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
         super().ingest(data)
         elementos = [data] if isinstance(data, dict) else data
         for elem in elementos:
             text = ": ".join(elem.values())
             self._items.append(text)
-        pass
 
 
-
-def main():
+def main() -> None:
     print("=== Code Nexus - Data Processor ===\n")
 
-    #NUMERIC PROCESSOR TEST
+    # NUMERIC PROCESSOR TEST
     print("Testing Numeric Processor...")
     numeric = NumericProcessor()
     print(" Trying to validate input '42': ", end="")
@@ -87,44 +88,52 @@ def main():
     print(" Trying to validate input 'Hello': ", end="")
     print(numeric.validate("Hello"))
     try:
-        print(" Test invalid ingestion of string 'foo' without prior validation:")
+        print(" Test invalid ingestion of string "
+              "'foo' without prior validation:")
         numeric.ingest("foo")
     except ValueError as ex:
-        print(" Got exception: ",ex)
+        print(" Got exception:", ex)
+
     num_list = [1, 2, 3, 4, 5]
     numeric.ingest(num_list)
-    print(" Processing data: ", num_list)
+    print(" Processing data:", num_list)
     print(" Extracting 3 values...")
-    for i in range(1, 4):
+    for _ in range(1, 4):
         num_tupla = numeric.output()
-        print(f" Numeric values {num_tupla[1]}: ", num_tupla[0])
+        if num_tupla:
+            print(f" Numeric values {num_tupla[1]}:", num_tupla[0])
 
-
-    #TEXT PROCESSOR TEST
+    # TEXT PROCESSOR TEST
     print("\nTesting Text Processor...")
     text = TextProcessor()
     print(" Trying to validate input '42': ", end="")
     print(text.validate(42))
     text_list = ["Hello", "Nexus", "World"]
     text.ingest(text_list)
-    print(" Processing data: ", text_list)
+    print(" Processing data:", text_list)
     print(" Extracting 1 value ...")
     tex_tupla = text.output()
-    print(f" Text value {tex_tupla[1]}: {tex_tupla[0]}")
+    if tex_tupla:
+        print(f" Text value {tex_tupla[1]}: {tex_tupla[0]}")
 
-
-    #LOG PROCESSOR TEST
-    print("\nTesting Log Porcessor...")
+    # LOG PROCESSOR TEST
+    print("\nTesting Log Processor...")
     log = LogProcessor()
     print(" Trying to validate input 'Hello': ", end="")
     print(log.validate('Hello'))
-    dic_list = [{'log_level': 'NOTICE', 'log_message': 'Connection to server'}, {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}]
+
+    dic_list = [
+        {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+    ]
     log.ingest(dic_list)
-    print(" Processing data: ", dic_list)
+    print(" Processing data:", dic_list)
     print(" Extracting 2 values...")
-    for i in range(1, 3):
+    for _ in range(1, 3):
         log_tupla = log.output()
-        print(f" log entry {log_tupla[1]}: {log_tupla[0]}")
+        if log_tupla:
+            print(f" log entry {log_tupla[1]}: {log_tupla[0]}")
+
 
 if __name__ == "__main__":
     main()
